77d870a6eb3646fd8de35990d3720d285e1d1bf8
18789432816 <18789432816@126.com>
2021-12-07 23:09:19 +0800

    流程页面--整改中

---
+++ src/views/work/oa/components/WorkbenchHeader.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="lg:flex">
+    <Avatar :src="stats.avatar || headerImg" :size="72" class="!mx-auto !block" />
+    <div class="md:ml-6 flex flex-col justify-center md:mt-0 mt-2">
+      <h1 class="md:text-lg text-md">{{ holle }}</h1>
+      <!-- <span class="text-secondary"> 今日晴，20℃ - 32℃！ </span> -->
+    </div>
+    <div class="flex flex-1 justify-end md:mt-0 mt-4">
+      <div class="flex flex-col justify-center text-right">
+        <span class="text-secondary"> 待办 </span>
+        <span class="text-2xl text-center">2</span>
+      </div>
+
+      <div class="flex flex-col justify-center text-right md:ml-16 ml-12">
+        <span class="text-secondary"> 发起 </span>
+        <span class="text-2xl text-center">8</span>
+      </div>
+      <div class="flex flex-col justify-center text-right md:mx-16 mx-12">
+        <span class="text-secondary"> 已办 </span>
+        <span class="text-2xl text-center">8</span>
+      </div>
+      <div class="flex flex-col justify-center text-right md:mr-10 mr-4">
+        <span class="text-secondary"> 部门人数 </span>
+        <span class="text-2xl text-center">300</span>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+  import { computed, reactive } from 'vue';
+  import { Avatar } from 'ant-design-vue';
+  import { useUserStore } from '/@/store/modules/user';
+  import headerImg from '/@/assets/images/header.jpg';
+  import { useSystemStore } from '/@/store/modules/system';
+  import { SystemEnum } from '/@/enums/systemEnum';
+  import { formatToDate } from '/@/utils/dateUtil';
+
+  const userStore = useUserStore();
+  const userinfo = computed(() => userStore.getUserInfo);
+  const systemStore = useSystemStore();
+
+  const stats = reactive({
+    avatar: userinfo.value.imageUrl
+      ? systemStore.getSystemConfigMap[SystemEnum.SYSTEM_PATH] + userinfo.value.imageUrl
+      : headerImg,
+  });
+
+  const holle = computed(() => {
+    const h = Number(formatToDate(new Date(), 'HH'));
+    let holle;
+    if (0 <= h && h < 10) {
+      holle = '早上好,  ' + userinfo.value.userName + '  开始您一天的工作吧！';
+    } else if (10 <= h && h < 12) {
+      holle = '中午好,  ' + userinfo.value.userName + '  继续努力哟！';
+    } else if (12 <= h && h < 18) {
+      holle = '下午好,  ' + userinfo.value.userName + '  保持完美工作状态！';
+    } else if (18 <= h && h < 24) {
+      holle = '晚上好,  ' + userinfo.value.userName + '  早点休息吧！';
+    }
+    return holle;
+  });
+</script>
---
+++ src/views/work/oa/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <PageWrapper>
+    <template #headerContent><WorkbenchHeader /> </template>
+    <div class="lg:flex">
+      <div class="lg:w-7/10 w-full !mr-4 enter-y">
+        <div>分页</div>
+      </div>
+      <div class="lg:w-3/10 w-full enter-y">
+        <QuickNav />
+        <Card class="!my-4 enter-y">
+          <img class="xl:h-50 h-30 mx-auto" src="../../../assets/svg/illustration.svg" />
+        </Card>
+        <SaleRadar />
+      </div>
+    </div>
+  </PageWrapper>
+</template>
+<script lang="ts" setup>
+  import { Card } from 'ant-design-vue';
+  import { PageWrapper } from '/@/components/Page';
+  import QuickNav from './components/QuickNav.vue';
+  import WorkbenchHeader from './components/WorkbenchHeader.vue';
+  import SaleRadar from './components/SaleRadar.vue';
+</script>
