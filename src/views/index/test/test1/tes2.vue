c312c3d21925595e07288f898b95bd7de8a93d23
18789432816 <18789432816@126.com>
2022-02-17 01:23:22 +0800

    整改中

--- src/views/index/test/test1/tes2.vue
+++ src/views/index/test/test1/tes2.vue
@@ -6,75 +6,45 @@
 </template>
 
 <script lang="ts" setup>
-  import { inject } from 'vue';
   import { reactive, computed, watchEffect } from 'vue';
   import { Input, Button } from 'ant-design-vue';
-
-  const socket = inject('socket');
-
+  import VueSocketIO from 'vue-socket.io';
+  import { useUserStore } from '/@/store/modules/user';
+  const userStore = useUserStore();
   const stats = reactive({
     message: '',
   });
+
+  const socket = new VueSocketIO({
+    debug: true,
+    connection: 'http://localhost:9190',
+    options: {
+      path: '/sms-socket/socket.io',
+      transports: ['websocket', 'polling'],
+      query: {
+        Authorization: 'Bearer ' + userStore.getToken,
+      },
+    },
+  });
   console.log(socket);
 
-  socket.on('message_event', (data) => {
-    console.log('client has connected' + data);
-  });
-
-  socket.on('connect', () => {
+  socket.io.on('connect', () => {
     console.log('开启链接！！！');
   });
 
+  socket.io.on('message_event', (data) => {
+    console.log('client has connected' + data);
+  });
+
+  //const useSocket = useSocketStore();
+
   const onClick = () => {
     //const socket = useSocket.getSocket;
-    socket.emit('message_event', {
+    socket.io.emit('message_event', {
       msgType: 4,
       outType: 1,
       msgContent: stats.message,
     });
-
-    // import { reactive, computed, watchEffect } from 'vue';
-    // import { useWebSocket } from '@vueuse/core';
-    // import { useSocketStore } from '/@/store/modules/socket';
-    // import { Input, Button } from 'ant-design-vue';
-    // import io, { ManagerOptions } from 'socket.io-client';
-    // import VueSocketIO from 'vue-socket.io';
-    // import { useUserStore } from '/@/store/modules/user';
-    // const userStore = useUserStore();
-    // const stats = reactive({
-    //   message: '',
-    // });
-
-    // const socket = new VueSocketIO({
-    //   debug: true,
-    //   connection: 'http://localhost:9190',
-    //   options: {
-    //     path: '/sms-socket/socket.io',
-    //     transports: ['websocket', 'polling'],
-    //     query: {
-    //       Authorization: 'Bearer ' + userStore.getToken,
-    //     },
-    //   },
-    // });
-    // console.log(socket);
-
-    // socket.io.on('connect', () => {
-    //   console.log('开启链接！！！');
-    // });
-
-    // socket.io.on('message_event', (data) => {
-    //   console.log('client has connected' + data);
-    // });
-
-    // //const useSocket = useSocketStore();
-
-    // const onClick = () => {
-    //   //const socket = useSocket.getSocket;
-    //   socket.io.emit('message_event', {
-    //     msgType: 4,
-    //     outType: 1,
-    //     msgContent: stats.message,
-    //   });
   };
 </script>
 <style scoped>
