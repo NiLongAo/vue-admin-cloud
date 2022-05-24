3932365204f087a767623be12f5692281a36a32e
YuanYang <18789432816@126.com>
2022-05-24 16:32:32 +0800

    新增工具besa64 转 文件

--- src/utils/file/download.ts
+++ src/utils/file/download.ts
@@ -90,7 +90,20 @@ export function downloadByUrl({
   if (url.indexOf('?') === -1) {
     url += '?download';
   }
-
   openWindow(url, { target });
   return true;
 }
+
+export const dataURLtoFile = (base64, filename) => {
+  const arr = base64.split(','),
+    mime = arr[0].match(/:(.*?);/)[1],
+    bstr = atob(arr[1]);
+  let n = bstr.length;
+  const u8arr = new Uint8Array(n);
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n);
+  }
+  return new File([u8arr], filename, {
+    type: mime,
+  });
+};
