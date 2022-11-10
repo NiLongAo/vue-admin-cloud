c4c19fff33df9ffe17f15c7b6e29c8129e64e8f6
仝泽勇 <18789432816@126.com>
2022-11-10 11:44:19 +0800

    名称优化

--- src/settings/siteSetting.ts
+++ src/settings/siteSetting.ts
@@ -1,8 +1,8 @@
-// github repo url
-export const GITHUB_URL = 'https://github.com/anncwb/vue-vben-admin';
+// gitee repo url
+export const GITHUB_URL = 'https://gitee.com/nilongao/spring-boot-cloud';
 
 // vue-vben-admin-next-doc
 export const DOC_URL = 'https://vvbin.cn/doc-next/';
 
 // site url
-export const SITE_URL = 'https://vvbin.cn/next/';
+export const SITE_URL = 'https://www.nilongao.cn/';
