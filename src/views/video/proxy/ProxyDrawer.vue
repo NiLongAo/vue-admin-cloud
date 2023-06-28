bb43c801d06aceb492cfb08d26d6b2bd264a4951
仝泽勇 <18789432816@126.com>
2023-06-28 17:31:21 +0800

    部分提交

--- src/views/video/proxy/ProxyDrawer.vue
+++ src/views/video/proxy/ProxyDrawer.vue
@@ -17,39 +17,30 @@
   import { ref, computed, unref } from 'vue';
   import { BasicDrawer, useDrawerInner } from '/@/components/Drawer';
   import { BasicForm, FormSchema, useForm } from '/@/components/Form/index';
-  import { doPlatformDetail, doPlatformInsert,doPlatformUpdate } from '/@/api/video/platform';
-  import { TRANSPORT_TYPE_ENUM ,CHARSET_TYPE_ENUM,TREE_TYPE_ENUM} from '/@/enums/commonEnum';
+  import { doProxyDetail, doProxySave } from '/@/api/video/proxy';
+  import { PROXY_TYPE_ENUM,PROXY_RTP_TYPE_ENUM} from '/@/enums/commonEnum';
   import { useSystemStore } from '/@/store/modules/system';
-  import { isEmpty } from '/@/utils/is';
 
   const emit = defineEmits(['success', 'register']);
   const systemStore = useSystemStore();
   const isUpdate = ref(true);
-  const getTitle = computed(() => (!unref(isUpdate) ? '新增平台' : '编辑平台'));
+  const getTitle = computed(() => (!unref(isUpdate) ? '新增拉流' : '编辑拉流'));
 
 
   const handleOk = async () => {
     try {
-      const { enable,rtcp,ptz,startOfflinePush,asMessageChannel, ...values } = await validate();
-      if(unref(isUpdate)){
-        await doPlatformUpdate({
+      const { enable,enableAudio,enableMp4,enableRemoveNoneReader,enableDisableNoneReader,type,url,srcUrl, ...values } = await validate();
+      await doProxySave({
         ...values,
+        type:type,
+        url:type===1?url:'',
+        srcUrl:type===1?'':url,
         enable: enable === true ? 1 : 0,
-        rtcp: rtcp === true ? 1 : 0,
-        ptz: ptz === true ? 1 : 0,
-        startOfflinePush: startOfflinePush === true ? 1 : 0,
-        asMessageChannel: asMessageChannel === true ? 1 : 0,
+        enableAudio: enableAudio === true ? 1 : 0,
+        enableMp4: enableMp4 === true ? 1 : 0,
+        enableRemoveNoneReader: enableRemoveNoneReader === true ? 1 : 0,
+        enableDisableNoneReader: enableDisableNoneReader === true ? 1 : 0,
       });
-      }else{
-        await doPlatformInsert({
-        ...values,
-        enable: enable === true ? 1 : 0,
-        rtcp: rtcp === true ? 1 : 0,
-        ptz: ptz === true ? 1 : 0,
-        startOfflinePush: startOfflinePush === true ? 1 : 0,
-        asMessageChannel: asMessageChannel === true ? 1 : 0,
-      });
-      }
       setDrawerProps({ confirmLoading: true });
       closeDrawer();
       emit('success');
@@ -58,122 +49,37 @@
     }
   };
 
-  const transportTypes = computed(() => {
-    const template = systemStore.getDictMap[TRANSPORT_TYPE_ENUM];
+  const proxyTypes = computed(() => {
+    const template = systemStore.getDictMap[PROXY_TYPE_ENUM];
     const types = [] as any;
     Object.keys(template).forEach((key) => {
       types.push({ label: template[key], value: Number(key), key: Number(key) });
     });
     return types;
   });
-  const charsetTypes = computed(() => {
-    const template = systemStore.getDictMap[CHARSET_TYPE_ENUM];
+  const proxyRtpTypes = computed(() => {
+    const template = systemStore.getDictMap[PROXY_RTP_TYPE_ENUM];
     const types = [] as any;
     Object.keys(template).forEach((key) => {
       types.push({ label: template[key], value: Number(key), key: Number(key) });
     });
     return types;
   });
-  const treeTypes = computed(() => {
-    const template = systemStore.getDictMap[TREE_TYPE_ENUM];
-    const types = [] as any;
-    Object.keys(template).forEach((key) => {
-      types.push({ label: template[key], value: Number(key), key: Number(key) });
-    });
-    return types;
-  });
-
+  
   const schemasDevice: FormSchema[] = [
     {
-      field: 'id',
+      field: 'app',
       component: 'Input',
-      label: '编号',
-      show: false,
+      label: '应用名',
       colProps: {
         span: 12,
       },
       required: true,
     },
     {
-      field: 'serverGbId',
+      field: 'stream',
       component: 'Input',
-      label: 'SIP国标编码',
-      colProps: {
-        span: 12,
-      },
-      componentProps:{
-        onChange:(val) =>{
-          let {serverGbId:serverId,catalogId,administrativeDivision,serverGbDomain} = getFieldsValue();
-          const serverGbId= val?.target?.value;
-          if(val === serverId){
-            return;
-          }
-          if(isEmpty(catalogId) || catalogId === serverId){
-            setFieldsValue({
-              catalogId:serverGbId,
-            });
-          }
-          if(isEmpty(administrativeDivision) || administrativeDivision === serverId){
-            administrativeDivision = serverGbId?.length >=6 ?serverGbId.slice(0, 6):val?.target?.value;
-            setFieldsValue({
-              administrativeDivision:administrativeDivision,
-            });
-          }
-          if(isEmpty(serverGbDomain) || serverGbDomain === serverId){
-            serverGbDomain = serverGbId?.length >=6 ?serverGbId.slice(0, 6):val?.target?.value;
-            setFieldsValue({
-              serverGbDomain:serverGbDomain,
-            });
-          }
-        }
-      },
-      required: true,
-    },
-    {
-      field: 'serverGbDomain',
-      component: 'Input',
-      label: 'SIP国标域',
-      colProps: {
-        span: 12,
-      },
-      required: true,
-    },
-    {
-      field: 'serverIp',
-      component: 'Input',
-      label: 'SIP服务IP',
-      colProps: {
-        span: 12,
-      },
-      required: true,
-    },
-    {
-      field: 'serverPort',
-      component: 'InputNumber',
-      label: 'SIP服务端口',
-      colProps: {
-        span: 12,
-      },
-      componentProps: {
-        min:0,
-        max:65535
-      },
-      defaultValue: 0,
-      required: true,
-    },
-    {
-      field: 'username',
-      component: 'Input',
-      label: 'SIP认证账号',
-      colProps: {
-        span: 12,
-      },
-      required: true,
-    },
-    {
-      field: 'password',
-      component: 'Input',
-      label: 'SIP认证密码',
+      label: '流ID',
       colProps: {
         span: 12,
       },
@@ -182,187 +88,68 @@
     {
       field: 'name',
       component: 'Input',
-      label: 'SIP服务名称',
+      label: '名称',
       colProps: {
         span: 12,
       },
       required: true,
     },
     {
-      field: 'administrativeDivision',
+      field: 'name',
       component: 'Input',
-      label: '行政区划',
+      label: '名称',
       colProps: {
         span: 12,
       },
+      required: true,
     },
     {
-      field: 'catalogId',
+      field: 'url',
       component: 'Input',
-      label: '目录编号',
+      label: '拉流地址',
       colProps: {
         span: 12,
       },
+      required: true,
     },
     {
-      field: 'catalogGroup',
-      component: 'Select',
-      label: '目录分组',
+      field: 'timeoutMs',
+      component: 'InputNumber',
+      label: '超时时间(毫秒)',
       colProps: {
         span: 12,
       },
       componentProps: {
-        options: [
-          {
-            label:'1',
-            value:1,
-            key:'1',
-          },
-          {
-            label:'2',
-            value:2,
-            key:'2',
-          },
-          {
-            label:'4',
-            value:4,
-            key:'4',
-          },
-          {
-            label:'8',
-            value:8,
-            key:'8',
-          },
-        ],
+        min:1000,
+        max:60000
+      },
+      defaultValue: 10000,
+    },
+    {
+      field: 'type',
+      component: 'Select',
+      label: '拉流类型',
+      colProps: {
+        span: 12,
+      },
+      componentProps: {
+        options: proxyTypes,
       },
       defaultValue: 1,
       required: true,
     },
     {
-      field: 'deviceIp',
-      component: 'Input',
-      label: '设备IP',
-      colProps: {
-        span: 12,
-      },
-      required: true,
-    },
-    {
-      field: 'devicePort',
-      component: 'InputNumber',
-      label: '设备端口',
-      colProps: {
-        span: 12,
-      },
-      componentProps: {
-        min:0,
-        max:65535
-      },
-      defaultValue: 0,
-      required: true,
-    },
-    {
-      field: 'deviceGbId',
-      component: 'Input',
-      label: '设备国标编号',
-      colProps: {
-        span: 12,
-      },
-      required: true,
-    },
-    {
-      field: 'expires',
-      component: 'InputNumber',
-      label: '注册周期 (秒)',
-      colProps: {
-        span: 12,
-      },
-      componentProps: {
-        min:30,
-        max:68400
-      },
-      defaultValue: 30,
-      required: true,
-    },
-    {
-      field: 'keepTimeout',
-      component: 'InputNumber',
-      label: '心跳周期(秒)',
-      colProps: {
-        span: 12,
-      },
-      componentProps: {
-        min:30,
-        max:600
-      },
-      defaultValue: 30,
-      required: true
-    },
-    {
-      field: 'treeType',
+      field: 'rtpType',
       component: 'Select',
-      label: '树类型',
+      label: '拉流方式',
       colProps: {
         span: 12,
       },
       componentProps: {
-        options: treeTypes,
+        options: proxyRtpTypes,
       },
-      defaultValue: 215,
-      required: true,
-    },
-    {
-      field: 'transport',
-      component: 'Select',
-      label: '传输协议',
-      colProps: {
-        span: 12,
-      },
-      componentProps: {
-        options: transportTypes,
-      },
-      required: true,
       defaultValue: 1,
-    },
-    {
-      field: 'characterSet',
-      component: 'Select',
-      label: '字符集',
-      colProps: {
-        span: 12,
-      },
-      componentProps: {
-        options: charsetTypes,
-      },
-      defaultValue: 2,
       required: true,
-    },
-    {
-      field: 'ptz',
-      component: 'Switch',
-      label: '云台控制',
-      colProps: {
-        span: 12,
-      },
-      defaultValue: true,
-    },
-    {
-      field: 'rtcp',
-      component: 'Switch',
-      label: 'RTCP流保活',
-      colProps: {
-        span: 12,
-      },
-      defaultValue: false,
-    },
-    {
-      field: 'startOfflinePush',
-      component: 'Switch',
-      label: '主动拉流',
-      colProps: {
-        span: 12,
-      },
-      defaultValue: true,
     },
     {
       field: 'enable',
@@ -374,9 +161,37 @@
       defaultValue: true,
     },
     {
-      field: 'asMessageChannel',
+      field: 'enableAudio',
       component: 'Switch',
-      label: '消息通道',
+      label: '启用音频',
+      colProps: {
+        span: 12,
+      },
+      defaultValue: false,
+    },
+    {
+      field: 'enableMp4',
+      component: 'Switch',
+      label: '启用MP4',
+      colProps: {
+        span: 12,
+      },
+      defaultValue: false,
+    },
+    {
+      field: 'enableRemoveNoneReader',
+      component: 'Switch',
+      label: '无人观看删除',
+      colProps: {
+        span: 12,
+      },
+      defaultValue: false,
+    },
+    
+    {
+      field: 'enableDisableNoneReader',
+      component: 'Switch',
+      label: '无人观看停用',
       colProps: {
         span: 12,
       },
@@ -384,7 +199,7 @@
     },
   ];
 
-  const [registerDeviceForm, { setFieldsValue, validate,resetFields,getFieldsValue }] = useForm({
+  const [registerDeviceForm, { setFieldsValue, validate,resetFields }] = useForm({
     labelWidth: 120,
     schemas: schemasDevice,
     showActionButtonGroup: false,
@@ -396,14 +211,17 @@
     setDrawerProps({ confirmLoading: false });
     isUpdate.value = !!data?.isUpdate;
     if (unref(isUpdate)) {
-      const { enable,rtcp,ptz,startOfflinePush,asMessageChannel, ...entity } = await doPlatformDetail({ id: data.id });
+      const { enable,enableAudio,enableMp4,enableRemoveNoneReader,enableDisableNoneReader,type,url,srcUrl ,...entity } = await doProxyDetail({ id: data.id });
+      const proxyUrl =type===1?url:srcUrl;
       setFieldsValue({
         ...entity,
+        type:type,
+        url:proxyUrl,
         enable: enable === 1 ? true : false,
-        rtcp: rtcp === 1 ? true : false,
-        ptz: ptz === 1 ? true : false,
-        startOfflinePush: startOfflinePush === 1 ? true : false,
-        asMessageChannel: asMessageChannel === 1 ? true : false,
+        enableAudio: enableAudio === 1 ? true : false,
+        enableMp4: enableMp4 === 1 ? true : false,
+        enableRemoveNoneReader: enableRemoveNoneReader === 1 ? true : false,
+        enableDisableNoneReader: enableDisableNoneReader === 1 ? true : false,
       });
     }
   });
