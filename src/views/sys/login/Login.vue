89614a3d1404e7d5af43daaf6d7d99fe6ca4b281
仝泽勇 <18789432816@126.com>
2023-12-22 15:41:45 +0800

    部分逻辑优化

--- src/views/sys/login/Login.vue
+++ src/views/sys/login/Login.vue
@@ -106,12 +106,10 @@
       .app-iconify {
         color: #fff;
       }
-    }
 
-    input.fix-auto-fill,
-    .fix-auto-fill input {
-      -webkit-text-fill-color: #c9d1d9 !important;
-      box-shadow: inherit !important;
+      .ant-divider-inner-text {
+        color: @text-color-secondary;
+      }
     }
   }
 
@@ -119,6 +117,7 @@
     min-height: 100%;
     overflow: hidden;
 
+    /* stylelint-disable-next-line media-query-no-invalid */
     @media (max-width: @screen-xl) {
       background-color: #293146;
 
@@ -139,7 +138,7 @@
       background-repeat: no-repeat;
       background-position: 100%;
       background-size: auto 100%;
-
+      /* stylelint-disable-next-line media-query-no-invalid */
       @media (max-width: @screen-xl) {
         display: none;
       }
@@ -190,20 +189,20 @@
     }
 
     input:not([type='checkbox']) {
-      min-width: 270px;
-
+      min-width: 360px;
+      /* stylelint-disable-next-line media-query-no-invalid */
       @media (max-width: @screen-xl) {
-        min-width: 270px;
+        min-width: 320px;
       }
-
+      /* stylelint-disable-next-line media-query-no-invalid */
       @media (max-width: @screen-lg) {
         min-width: 260px;
       }
-
+      /* stylelint-disable-next-line media-query-no-invalid */
       @media (max-width: @screen-md) {
         min-width: 240px;
       }
-
+      /* stylelint-disable-next-line media-query-no-invalid */
       @media (max-width: @screen-sm) {
         min-width: 160px;
       }
@@ -211,11 +210,6 @@
 
     .@{countdown-prefix-cls} input {
       min-width: unset;
-    }
-
-    .ant-divider-inner-text {
-      color: @text-color-secondary;
-      font-size: 12px;
     }
   }
 </style>
